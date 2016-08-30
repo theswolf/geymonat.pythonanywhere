1179ca0396e3354cdc9c1304e2bcac9537685592
christian <chris.geymo@gmail.com>
2016-08-30 12:29:56 +0200

    startup

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,19 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello from Flask!'
+    
+@app.route('/details')
+def hello_world():
+    return url_for('static',filename='style.css')
+    
+if __name__ == '__main__':
+	app.debug = True
+	app.run()
 
